52de759de2f170b5c852df3ca71d325009fa6b7e
Karol Basilico <116791261+basilicokarol@users.noreply.github.com>
2024-02-10 10:47:53 +0100

    Add files via upload

---
+++ Models/database.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Prenota.Models;
+
+public class dbContext : DbContext
+    {
+        private readonly DbContextOptions? _options;
+        public dbContext(){}
+
+        protected override void 
+                OnConfiguring(DbContextOptionsBuilder options)
+                => options.UseSqlite("Data Source=database.db");
+
+        public DbSet<Utente> Utenti { get ; set; }
+    }
